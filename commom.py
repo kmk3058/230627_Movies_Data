a827bcdbbcf3086897751b43db2b6e72f47a9a1b
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-27 16:49:27 +0900

    update

--- commom.py
+++ commom.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
 def get_2022():
     data_2022 = pd.read_csv("./kobis_data_2021.csv")
     return data_2022
-
+03
 def get_2021():
     data_2021 = pd.read_csv("./kobis_data_2022.csv")
     return data_2021
