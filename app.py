4dbc6a3777a5d8addc34378e60b20d307ab980e8
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-27 16:33:51 +0900

    first_01

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import common
 
-common.page_config()
+# common.page_config()
 
 st.title("21-22년 기간별 박스오피스")
-# st.caption("""
-# "
-# """)
+
 st.image("img/box_office.jpg")
 
 # st.write("[Data](/Data)")
