6d8a358c4df0caeacff8c8a8a1672dd3c2200827
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 07:54:53 +0900

    ta3 tab4

--- pages/03. 2021_sales_top10.py
+++ pages/03. 2021_sales_top10.py
@@ -39,3 +39,52 @@ with tab2:
 
     # Display the plot using Streamlit
     st.plotly_chart(fig)
+
+tab3, tab4 = st.tabs(["Country", "Genre"])
+
+with tab3:
+    # 데이터프레임 생성
+    data = {
+        'Year': ['2021'] * len(sale_grp_2021) + ['2022'] * len(sale_grp_2022),
+        '국가': list(sale_grp_2021.index) + list(sale_grp_2022.index),
+        '매출(단위 억원)': list(sale_grp_2021.values // 100_000_000) + list(sale_grp_2022.values // 100_000_000)
+    }
+
+    # Plotly Express를 사용하여 막대 그래프 생성
+    fig = px.bar(data_frame=data, x='국가', y='매출(단위 억원)', color='Year',
+                labels={'국가': '국가', '매출(단위 억원)': '매출(단위 억원)', 'Year': '연도'})
+
+    # 그래프 레이아웃 설정
+    fig.update_layout(
+        title='국가별 매출 성장률',
+        xaxis_tickangle=-45,
+        legend_title='연도'
+    )
+
+    # Streamlit에서 그래프 출력
+    st.plotly_chart(fig)
+
+with tab4:
+    gnr_repl_20021 = df_2021['대표장르']
+    gnr_grp_2021 = gnr_repl_20021.groupby(gnr_repl_20021.values).count()
+
+        # 데이터프레임 생성
+    data = {
+        'Year': ['2021'] * len(gnr_grp_2021) + ['2022'] * len(gnr_grp_2022),
+        '장르': list(gnr_grp_2021.index) + list(gnr_grp_2022.index),
+        '상영수': list(gnr_grp_2021.values) + list(gnr_grp_2022.values)
+    }
+
+    # Plotly Express를 사용하여 막대 그래프 생성
+    fig = px.bar(data_frame=data, x='장르', y='상영수', color='Year',
+                labels={'장르': '장르', '상영수': '상영수', 'Year': '연도'})
+
+    # 그래프 레이아웃 설정
+    fig.update_layout(
+        title='장르별 상영수',
+        xaxis_tickangle=-45,
+        legend_title='연도'
+    )
+
+    # Streamlit에서 그래프 출력
+    st.plotly_chart(fig)
