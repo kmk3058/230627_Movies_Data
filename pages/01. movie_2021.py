7e6f5c5ba789045a7640fcd579a9b6a3cef310fd
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-27 16:54:05 +0900

    data_input

--- pages/01. movie_2021.py
+++ pages/01. movie_2021.py
@@ -1,3 +1,8 @@
 import streamlit as st
 import commmon
 
+common.page_config()
+st.title("Data")
+st.dataframe(common.get_2021(),
+             use_container_width=True,
+             hide_index=True)
---
+++ pages/02. movie_2022.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import commmon
+
+common.page_config()
+st.title("Data")
+st.dataframe(common.get_2022(),
+             use_container_width=True,
+             hide_index=True)
