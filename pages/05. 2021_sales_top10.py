37769293daa283e79b7ba2b5395ea851fdcfd40c
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 06:47:24 +0900

    color

--- pages/05. 2021_sales_top10.py
+++ pages/05. 2021_sales_top10.py
@@ -11,7 +11,8 @@ st.title("2022년 박스오피스 Top 10")
 
 df = common.get_2022()
 
-# genre_counts = df['Genre'].value_counts().sort_values(ascending=False)
+# Define custom colors
+custom_colors = ['#FF6F61', '#6B5B95', '#88B04B', '#F7CAC9', '#92A8D1', '#955251', '#B565A7', '#009B77', '#DD4124', '#D65076']
 
 tab1, tab2 = st.tabs(["Sales", "Numbers"])
 
@@ -20,7 +21,7 @@ with tab1:
     sales_top10 = df[['영화명', '매출액']].sort_values(by='매출액', ascending=False).head(10)
 
     # Create the bar plot using Plotly Express
-    fig = px.bar(sales_top10, x='영화명', y='매출액', labels={'영화명': '영화명', '매출액': '매출액'})
+    fig = px.bar(sales_top10, x='영화명', y='매출액', labels={'영화명': '영화명', '매출액': '매출액'}, color='영화명', color_discrete_sequence=custom_colors)
     fig.update_yaxes(tickformat="~s", ticksuffix="원")
     fig.update_layout(title_text='2022년 매출액 top 10')
 
@@ -32,7 +33,7 @@ with tab2:
     audience_top10 = df[['영화명', '관객수']].sort_values(by='관객수', ascending=False).head(10)
 
     # Create the bar plot using Plotly Express
-    fig = px.bar(audience_top10, x='영화명', y='관객수', labels={'영화명': '영화명', '관객수': '관객수'})
+    fig = px.bar(audience_top10, x='영화명', y='관객수', labels={'영화명': '영화명', '관객수': '관객수'}, color='영화명', color_discrete_sequence=custom_colors)
     fig.update_yaxes(tickformat="~s", ticksuffix="명")
     fig.update_layout(title_text='2022년 관객수 top 10')
 
