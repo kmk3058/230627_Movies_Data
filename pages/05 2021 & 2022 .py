f3cf3444f40a3d9790a27a2a864cec39207698bf
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 11:52:11 +0900

    check

--- pages/05 2021 & 2022 .py
+++ pages/05 2021 & 2022 .py
@@ -149,63 +149,142 @@ with tab4:
     st.plotly_chart(fig)
 
 
-gc_2021 = df_2021.loc[:,'대표장르'].value_counts()
-gc_2022 = df_2022.loc[:,'대표장르'].value_counts()
-tab_menus = ["2021", "2022"]
-tab5, tab6 = st.tabs(tab_menus)
+# gc_2021 = df_2021.loc[:,'대표장르'].value_counts()
+# gc_2022 = df_2022.loc[:,'대표장르'].value_counts()
+# tab_menus = ["2021", "2022"]
+# tab5, tab6 = st.tabs(tab_menus)
 
-with tab5:
+# with tab5:
     
-    from pywaffle import Waffle
+#     from pywaffle import Waffle
 
-    gc_2021_modified = {}
+#     gc_2021_modified = {}
 
-    for key, value in gc_2021.items():
-        gc_2021_modified[key] = value // 20
-    fig = plt.figure(
-        FigureClass=Waffle,
-        plots={
-            111: {
-                'values': gc_2021_modified,
-                'labels': ["{0} ({1})".format(n, v) for n, v in gc_2021.items()],
-                'legend': {'loc': 'upper left', 'bbox_to_anchor': (1, 1), 'fontsize': 8},
-                'title': {'label': '2021 장르 (20개당 별 하나)', 'loc': 'left', 'fontproperties' : fontprop}
-            }
-        },
-        rows=15,
-        figsize=(15, 5),
-        font_size=15,
-        icons ='star'
+#     for key, value in gc_2021.items():
+#         gc_2021_modified[key] = value // 20
+#     fig = plt.figure(
+#         FigureClass=Waffle,
+#         plots={
+#             111: {
+#                 'values': gc_2021_modified,
+#                 'labels': ["{0} ({1})".format(n, v) for n, v in gc_2021.items()],
+#                 'legend': {'loc': 'upper left', 'bbox_to_anchor': (1, 1), 'fontsize': 8},
+#                 'title': {'label': '2021 장르 (20개당 별 하나)', 'loc': 'left', 'fontproperties' : fontprop}
+#             }
+#         },
+#         rows=15,
+#         figsize=(15, 5),
+#         font_size=15,
+#         icons ='star'
+#     )
+#     plt.show()
+#     st.pyplot(fig)
+
+# with tab6:
+
+#     # 한글 폰트 설정
+#     font_path = './NanumGothic.ttf'  # 한글 폰트 파일 경로
+#     fontprop = fm.FontProperties(fname=font_path)
+#     plt.rc('font', family=fontprop.get_name())
+
+#     from pywaffle import Waffle
+
+#     gc_2022_modified = {}
+#     for key, value in gc_2022.items():
+#         gc_2022_modified[key] = value // 20
+#     fig = plt.figure(
+#         FigureClass=Waffle,
+#         plots={
+#             111: {
+#                 'values': gc_2022_modified,
+#                 'labels': ["{0} ({1})".format(n, v) for n, v in gc_2022.items()],
+#                 'legend': {'loc': 'upper left', 'bbox_to_anchor': (1, 1), 'fontsize': 8},
+#                 'title': {'label': '2022 장르 (20개당 별 하나)', 'loc': 'left', 'fontproperties' : fontprop}
+#             }
+#         },
+#         rows=15,
+#         figsize=(15, 5),
+#         font_size=15,
+#         icons='star'
+#     )
+#     plt.show()
+#     st.pyplot(fig)
+
+
+data_2021 = common.get_2021()
+data_2022 = common.get_2022()
+top10_2021 = data_2021.head(10)
+top10_2022 = data_2022.head(10)
+# top10_2021_sorted = top10_2021.sort_values(by='매출액', ascending=False)
+# top10_2022_sorted = top10_2022.sort_values(by='매출액', ascending=False)
+# index = np.arange(len(top10_2021_sorted))
+# w = 0.4
+# 순위 = [f"{i+1}위" for i in range(10)]
+# plt.title('2021년과 2022년 TOP10 매출 비교', fontproperties=font_prop)
+# plt.bar(index - w/2, top10_2021_sorted['매출액'], width=w, label='2021', color='#FFD0D0')
+# plt.bar(index + w/2, top10_2022_sorted['매출액'], width=w, label='2022', color='#3AA6B9')
+# plt.xticks(index, 순위, fontproperties=font_prop)
+# # y축의 형식을 변경하기 위해 ticker를 사용합니다.
+# plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f'{x/1e8:.0f}억 원'))
+# # y축 레이블에 폰트 적용
+# plt.gca().set_yticklabels(plt.gca().get_yticklabels(), fontproperties=font_prop)
+# plt.legend()
+# st.pyplot(plt.gcf())
+tab_names = ['매출 비교', '관객 수 비교']
+active_tab = st.sidebar.radio('선택', tab_names)
+if active_tab == '매출 비교':
+    top10_2021_sorted = data_2021.sort_values(by='매출액', ascending=False).head(10)
+    top10_2022_sorted = data_2022.sort_values(by='매출액', ascending=False).head(10)
+    x = top10_2021_sorted['영화명']
+    y1 = top10_2021_sorted['매출액']
+    y2 = top10_2022_sorted['매출액']
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=x, y=y1, name='2021', marker_color='#FFD0D0'))
+    fig.add_trace(go.Bar(x=x, y=y2, name='2022', marker_color='#3AA6B9'))
+    fig.update_layout(
+        title={
+        'text': '2021년과 2022년 TOP10 매출 비교',
+        'font': {'size': 20},
+        'x': 0.25
+    },
+                    # xaxis_tickangle=-45,
+        yaxis=dict(
+            ticksuffix='원',
+            tickfont=dict(size=15)
+        ),
+        xaxis=dict(
+            tickmode='array',
+            tickvals=[i for i in range(10)],
+            ticktext=[f'{i+1}위' for i in range(10)],  # x축의 틱 레이블을 1위에서 10위로 표시
+            tickfont=dict(size=15)
+        ),
+        # legend=dict(
+        #     x=0.5,
+        #     y=1.1,
+        #     orientation='h'
+        # ),
     )
+    st.plotly_chart(fig)
+elif active_tab == '관객 수 비교':
+    value_2021 = data_2021['관객수'].sum()
+    value_2022 = data_2022['관객수'].sum()
+    total = value_2021 + value_2022  # 값의 합계 계산
+    values = [value_2021, value_2022]
+    labels = [f'2021년\n{value_2021:,}명', f'2022년\n{value_2022:,}명']
+    colors = ['#FFD0D0', '#3AA6B9']
+    plt.figure(figsize=(3.5,3.5),dpi= 300)
+    plt.pie(values, labels=labels, autopct='%0.1f%%', colors=colors, startangle=90, textprops = {"fontproperties" : font_prop})
+    plt.title('2021년 vs 2022년 TOP10 관객수 비교' , fontproperties = font_prop)
+    plt.axis('equal')
     plt.show()
-    st.pyplot(fig)
-
-with tab6:
-
-    # 한글 폰트 설정
-    font_path = './NanumGothic.ttf'  # 한글 폰트 파일 경로
-    fontprop = fm.FontProperties(fname=font_path)
-    plt.rc('font', family=fontprop.get_name())
-
-    from pywaffle import Waffle
-
-    gc_2022_modified = {}
-    for key, value in gc_2022.items():
-        gc_2022_modified[key] = value // 20
-    fig = plt.figure(
-        FigureClass=Waffle,
-        plots={
-            111: {
-                'values': gc_2022_modified,
-                'labels': ["{0} ({1})".format(n, v) for n, v in gc_2022.items()],
-                'legend': {'loc': 'upper left', 'bbox_to_anchor': (1, 1), 'fontsize': 8},
-                'title': {'label': '2022 장르 (20개당 별 하나)', 'loc': 'left', 'fontproperties' : fontprop}
-            }
-        },
-        rows=15,
-        figsize=(15, 5),
-        font_size=15,
-        icons='star'
-    )
-    plt.show()
-    st.pyplot(fig)
+    st.pyplot(plt.gcf())
+# value_2021 = data_2021['관객수'].sum()
+# value_2022 = data_2022['관객수'].sum()
+# total = value_2021 + value_2022  # 값의 합계 계산
+# values = [value_2021, value_2022]
+# labels = [f'2021년\n{value_2021:,}', f'2022년\n{value_2022:,}']
+# colors = ['#FFD0D0', '#3AA6B9']
+# fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='percent', marker=dict(colors=colors))])
+# fig.update_layout(title='2021년 vs 2022년 TOP10 관객수 비교\n총 관객수: {:,}'.format(total))
+# # Display the pie chart using Streamlit
+# st.plotly_chart(fig)
