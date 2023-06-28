82605818fa19c63c56b90dc20ed407594e05e2c9
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 14:23:18 +0900

    st.divider()

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ common.page_config()
 
 st.title("2021-2022년 한국 박스오피스 영화")
 st.image("img/box_office.jpg")
-st.write('-')
+st.divider()
 # 영화 산업 데이터 분석 프로젝트 소개
 st.markdown("### 🎬 Page 소개")
 st.markdown("#### 🎞 RAW_DATA")
@@ -14,7 +14,7 @@ st.markdown("#### 🎟 TOP_10")
 st.markdown("🔉2021/2022년도 각 년도 매출액과 관객수 TOP 10")
 st.markdown("#### 🎫 2021_2022")
 st.markdown("🔊2021 대비 2022년도 매출액, 관객수 변화 추이, 특정 장르/국가별 영화 관객수 등")
-st.write('-')
+st.divider()
 st.markdown("### 🎬 21/22 한국 박스오피스 미리보기")
 if st.button("2021년", key='2021'):
     tabs = st.tabs(["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"])
