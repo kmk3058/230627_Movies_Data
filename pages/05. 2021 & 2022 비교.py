3d988ee9370c8b4a87f49853556a87542b15ccea
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 09:43:36 +0900

    update

--- pages/05. 2021 & 2022 비교.py
+++ pages/05. 2021 & 2022 비교.py
@@ -54,26 +54,23 @@ with tab2:
     gnr_grp_2022 = gnr_repl_2022.groupby(gnr_repl_2022.values).count()
 
     # 데이터프레임 생성
-    data_2021 = {
-        '장르': list(gnr_grp_2021.index),
-        '상영수': list(gnr_grp_2021.values)
+    data = {
+    'Year': ['2021'] * len(gnr_grp_2021) + ['2022'] * len(gnr_grp_2022),
+    '장르': list(gnr_grp_2021.index) + list(gnr_grp_2022.index),
+    '상영수': list(gnr_grp_2021.values) + list(gnr_grp_2022.values)
     }
 
-    data_2022 = {
-        '장르': list(gnr_grp_2022.index),
-        '상영수': list(gnr_grp_2022.values)
-    }
+    # Plotly Express를 사용하여 막대 그래프 생성
+    fig = px.bar(data_frame=data, x='장르', y='상영수', color='Year',
+                labels={'장르': '장르', '상영수': '상영수', 'Year': '연도'})
 
-    # 2021년 데이터 그래프 생성
-    fig_2021 = px.bar(data_frame=data_2021, x='장르', y='상영수',
-                    labels={'장르': '장르', '상영수': '상영수'}, color_discrete_sequence=['#FF6F61'])
+    # 그래프 레이아웃 설정
+    fig.update_layout(
+    title='장르별 상영수',
+    xaxis_tickangle=-45,
+    legend_title='연도',
+    bargap=0.4  # 막대 간격 조정 (기본값은 0.2)
+    )
 
-    # 2022년 데이터 그래프 생성
-    fig_2022 = px.bar(data_frame=data_2022, x='장르', y='상영수',
-                    labels={'장르': '장르', '상영수': '상영수'}, color_discrete_sequence=['#6B5B95'])
-
-    # 2021년 그래프 출력
-    st.plotly_chart(fig_2021)
-
-    # 2022년 그래프 출력
-    st.plotly_chart(fig_2022)
+    # Streamlit에서 그래프 출력
+    st.plotly_chart(fig)
