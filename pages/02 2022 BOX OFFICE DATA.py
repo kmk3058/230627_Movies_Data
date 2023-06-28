205b7389cc64cf26cd3e9b4f18c25556e616bb14
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 12:05:11 +0900

    edit raw data

--- pages/02 2022 BOX OFFICE DATA.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import common
-
-common.page_config()
-st.title("2022년 박스오피스 Data")
-st.dataframe(common.get_2022(),
-             use_container_width=True,
-             hide_index=True)
