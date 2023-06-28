346181247d9a6c2d9ce15380dac0e4cf7d6c1606
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 14:01:20 +0900

    d

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ st.title("2021-2022년 한국 박스오피스")
 if st.button("2021년", key='2021'):
     tabs = st.tabs(["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"])
     for i, tab in enumerate(tabs):
-        tab.image(f"./img/2021/{i+1}.png")
+        tab.image(f"./img/2021/{i+1}.png", width=300)
 
 if st.button("2022년", key='2022'):
     tabs = st.tabs(["1st", "2nd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"])
     for i, tab in enumerate(tabs):
-        tab.image(f"./img/2022/{i+1}.jpg")
+        tab.image(f"./img/2022/{i+1}.jpg", width=300)
 
