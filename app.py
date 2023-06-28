003736ea28fea5dff6e14976f047a62c3e37bdd0
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 11:02:51 +0900

    page 저장

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ year = st.selectbox("년도 선택", ['2021년', '2022년'])
 
 if year == '2021년':
     df = common.get_2021()  # Get 2021 data
-    next_page = 'https://230627movie-nh972165qyg.streamlit.app/._2021_sales_top10?next_page=..%2Fpages%2F04.+2022_sales_top10.py'
+    next_page_df = common.get_2021_top10()  # Get top 10 data for 2021
 else:
     df = common.get_2022()  # Get 2022 data
-    next_page = 'https://230627movie-nh972165qyg.streamlit.app/._2022_sales_top10?next_page=..%2Fpages%2F04.+2022_sales_top10.py'
+    next_page_df = common.get_2022_top10()  # Get top 10 data for 2022
 
 # Show the selected year's data
 st.write(f"{year} 데이터:")
@@ -24,8 +24,18 @@ st.write(df)
 # Visualization on the next page
 if st.button("해당 년도 페이지로"):
     # Store the selected year's data in session state
-    st.session_state['selected_year_data'] = df
+    st.session_state['selected_year_data'] = next_page_df
     # Store the selected year in session state
     st.session_state['selected_year'] = year
-    # Open the next page in a new tab
-    st.markdown(f'<a href="{next_page}" target="_blank">Go to {year} 페이지</a>', unsafe_allow_html=True)
+
+# Next page
+if 'selected_year_data' in st.session_state:
+    selected_year_data = st.session_state['selected_year_data']
+    selected_year = st.session_state['selected_year']
+    
+    if selected_year == '2021년':
+        st.write("2021년 페이지:")
+        st.write(selected_year_data)
+    else:
+        st.write("2022년 페이지:")
+        st.write(selected_year_data)
