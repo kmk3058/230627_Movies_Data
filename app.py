c28f7d3638b103833d063c294e992bc889f8aa0a
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 14:15:55 +0900

    d

--- app.py
+++ app.py
@@ -9,17 +9,14 @@ st.image("img/box_office.jpg")
 
 # 영화 산업 데이터 분석 프로젝트 소개
 st.markdown("### Page 소개")
-st.markdown("#### RAW_DATA 영화의 제목, 매출액, 관객수, 대표장르, 대표국적 등의 정보")
-st.markdown("영화의 제목, 매출액, 관객수, 대표장르, 대표국적 등의 정보")
-st.markdown("TOP_10 : 2021/2022년도 각 년도 매출액과 관객수 TOP 10")
-st.markdown("2021_2022 : 2021 대비 2022년도 매출액, 관객수 변화 추이, 특정 장르/국가별 영화 관객수 등 ")
+st.markdown("#### RAW_DATA")
+st.markdown("🎞 영화의 제목, 매출액, 관객수, 대표장르, 대표국적 등의 정보")
+st.markdown("#### TOP_10") 
+st.markdown("🎟 2021/2022년도 각 년도 매출액과 관객수 TOP 10")
+st.markdown("####2021_2022")
+st.markdown("🎫 2021 대비 2022년도 매출액, 관객수 변화 추이, 특정 장르/국가별 영화 관객수 등 ")
 
-import streamlit as st
-
-st.sidebar.title("박스오피스 데이터")
-# year = st.sidebar.radio("년도 선택", ['2021', '2022'])
-
-st.title("2021-2022년 한국 박스오피스")
+st.markdown("#### 21/22 박스오피스 미리보기")
 
 if st.button("2021년", key='2021'):
     tabs = st.tabs(["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"])
