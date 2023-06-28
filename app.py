53029ecc23ef13181dc2ca18c157527c19abad26
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 14:17:13 +0900

    img

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ st.image("img/box_office.jpg")
 
 # 영화 산업 데이터 분석 프로젝트 소개
 st.markdown("### Page 소개")
-st.markdown("#### RAW_DATA")
-st.markdown("🎞 영화의 제목, 매출액, 관객수, 대표장르, 대표국적 등의 정보")
-st.markdown("#### TOP_10") 
-st.markdown("🎟 2021/2022년도 각 년도 매출액과 관객수 TOP 10")
-st.markdown("####2021_2022")
-st.markdown("🎫 2021 대비 2022년도 매출액, 관객수 변화 추이, 특정 장르/국가별 영화 관객수 등 ")
+st.markdown("#### 🎞RAW_DATA")
+st.markdown("🔈영화의 제목, 매출액, 관객수, 대표장르, 대표국적 등의 정보")
+st.markdown("#### 🎟TOP_10") 
+st.markdown("🔉2021/2022년도 각 년도 매출액과 관객수 TOP 10")
+st.markdown("#### 🎫2021_2022")
+st.markdown("🔊2021 대비 2022년도 매출액, 관객수 변화 추이, 특정 장르/국가별 영화 관객수 등")
 
 st.markdown("#### 21/22 박스오피스 미리보기")
 
