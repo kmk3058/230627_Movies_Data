5f3a2278f8d62bef827dd91eb189392c25a537aa
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 10:56:57 +0900

    data 수정

--- common.py
+++ common.py
@@ -6,11 +6,11 @@ import seaborn as sns
 
 def get_2021():
     data_2021 = pd.read_csv("./kobis_data_2021.csv")
-    return data_2021[1:, :]
+    return data_2021
 
 def get_2022():
     data_2022 = pd.read_csv("./kobis_data_2022.csv")
-    return data_2022[1:, :]
+    return data_2022
 
 def page_config():
     st.set_page_config(
