aaa7a62a91587a0a5f7d88f39446ba51346461d8
kmk3058 <118182800+kmk3058@users.noreply.github.com>
2023-06-28 13:37:53 +0900

    edit

--- pages/01. RAW_DATA.py
+++ pages/01. RAW_DATA.py
@@ -10,6 +10,10 @@ year = st.sidebar.radio("년도 선택", ['2021년', '2022년'])
 # Display selected year's data
 st.title(f"{year} 박스오피스 Data")
 if year == '2021년':
-    st.dataframe(common.get_2021(), use_container_width=True, hide_index=True)
+    df_2021 = common.get_2021()
+    df_2021 = df_2021.iloc[:, 1:]  # Exclude the first column
+    st.dataframe(df_2021, use_container_width=True, hide_index=True)
 else:
-    st.dataframe(common.get_2022(), use_container_width=True, hide_index=True)
+    df_2022 = common.get_2022()
+    df_2022 = df_2022.iloc[:, 1:]  # Exclude the first column
+    st.dataframe(df_2022, use_container_width=True, hide_index=True)
